c3f4b8d34e6f484b738ea7ee0a40aecf7fbf5527
Grigala <76661178+R-Grigala@users.noreply.github.com>
2022-07-21 16:57:23 +0400

    Update settings.py

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fz&j3n!g614q^(s6#3nzd1uy+jmy8hl_$^m1pex609y&qr!hc('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['quick-quiz-rg.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['https://quiziumer.herokuapp.com/','127.0.0.1']
 
 # Application definition
 
