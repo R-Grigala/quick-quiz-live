42fc9acf09e4beaa5229a99180c5024bb0823a64
geogamura <grigalashvili.r1999@gmail.com>
2022-07-26 16:04:49 +0400

    new

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR /'db.sqlite3',
+#     }
+# }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
