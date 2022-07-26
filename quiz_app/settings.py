ff3650f9d188ced790bd35df6f95a36ebe2f7f84
Grigala <76661178+R-Grigala@users.noreply.github.com>
2022-07-26 14:52:16 +0400

    comment database

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -99,6 +99,7 @@ WSGI_APPLICATION = 'quiz_app.wsgi.application'
 #     }
 
 # }
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
