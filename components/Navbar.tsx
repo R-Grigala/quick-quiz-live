6fcab12f3255c7345e93dd1952c445637f24bbef
R-Grigala <r.grigalashvili777@gmail.com>
2024-02-02 18:13:36 +0400

    Coding Grigala://  - _ -

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='border-2 border-red-500 flexBeatween max-container padding-container relative z-30 py-5'>
-        <Link href={"/"}>
-            Home
+    <nav className='flexBeatween max-container padding-container relative z-30 py-5'>
+        <Link href="/">
+            <Image src="/hilink-logo.svg" alt='logo' width={74} height={29}/>
         </Link>
     </nav>
   )
