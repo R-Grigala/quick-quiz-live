5c1921613ad197575b58aa70ca89997ebf0426dc
R-Grigala <r.grigalashvili777@gmail.com>
2024-02-17 18:48:33 +0400

    Coding Finished added all components

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
-import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '@/constants';
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
@@ -13,11 +13,11 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between mb:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn key={index} title={columns.title}>
                 <ul className='regular-14 flex flex-col gap-4 text-gray-30'>
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, linkIndex) => (
+                    <Link href="/" key={linkIndex}>
                       {link}
                     </Link>
                   ))}
@@ -26,15 +26,38 @@ const Footer = () => {
             ))}
 
             <div className='flex flex-col gap-5'>
-              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
-                  <Link href="/">
+              <FooterColumn key={FOOTER_CONTACT_INFO.title} title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link, linkIndex) => (
+                  <Link
+                    href="/"
+                    key={linkIndex}
+                    className='flex gap-4 md:flex-col lg:flex-row'
+                  >
+                    <p className='whitespace-nowrap'>
+                      {link.label}:
+                    </p>
+                    <p className='medium-14 whitespace-nowrap text-blue-70'>
+                      {link.value}
+                    </p>
                   </Link>
                 ))}
               </FooterColumn>
             </div>
+            <div className='flex flex-col gap-5'>
+              <FooterColumn key={SOCIALS.title} title={SOCIALS.title}>
+                <ul className='regular-14 flex gap-4 text-gray-30'>
+                  {SOCIALS.links.map((link, linkIndex) => (
+                    <Link href="/" key={linkIndex}>
+                      <Image src={link} alt='logo' width={24} height={24}/>
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
           </div>
         </div>
+        <div className='border bg-gray-20'/>
+        <p className='regular-14 w-full text-center text-gray-30'>2024 OfDigital | All rights reserved</p>
       </div>
     </footer>
   )
@@ -53,6 +76,5 @@ const FooterColumn = ({ title, children }: FooterColumnProps) => {
     </div>
   )
 }
-
 
 export default Footer;
